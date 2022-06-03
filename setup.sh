eb057718a5ef91dcbfce0fc234df55fbdf0c25db
Mridul Sharma <mr.mridhulsharma@gmail.com>
2022-06-03 20:11:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
 [theme]
 base="dark"
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
