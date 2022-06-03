e773470e29b36a6fc5979568eaaec2a8d2756ef0
Mridul Sharma <mr.mridhulsharma@gmail.com>
2022-06-03 22:34:27 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
-mkdir -p %userprofile%/.streamlit/
+mkdir -p %Mridul-Sharma01%/.streamlit/
 echo "[theme]
 primaryColor = "#FF4B4B"
 backgroundColor = "#0E1117"
 secondaryBackgroundColor = "#262730"
 textColor= "#FAFAFA"
 font = "sans serif"
-" > %userprofile%/.streamlit/credentials.toml
+" > %Mridul-Sharma01%/.streamlit/credentials.toml
 [server]
 headless = true
 enableCORS=false
 port = 80
-" > %userprofile%/.streamlit/config.toml
+" > %Mridul-Sharma01%/.streamlit/config.toml
 
 
