f6188d94d731d3c15e6c60cde74deba9a2c87ded
Mridul Sharma <mr.mridhulsharma@gmail.com>
2022-06-03 22:06:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "[theme]
-primaryColor = ‘#FF4B4B’
-backgroundColor = ‘#0E1117’
-secondaryBackgroundColor = ‘#262730’
-textColor= ‘#FAFAFA’
-font = ‘sans serif’
+primaryColor = "#FF4B4B"
+backgroundColor = "#0E1117"
+secondaryBackgroundColor = "#262730"
+textColor= "#FAFAFA"
+font = "sans serif"
+" > ~/.streamlit/credentials.toml
 [server]
 headless = true
 enableCORS=false
