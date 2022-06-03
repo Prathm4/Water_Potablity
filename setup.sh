a969aaec2c938c718e316ff93e727ca742f7a86a
Mridul Sharma <mr.mridhulsharma@gmail.com>
2022-06-03 21:17:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-[theme]\n
-base=\"dark"\n
+echo "[theme]
+base= 'dark'
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+[server]
+headless = true
+enableCORS=false
+port = $PORT
 " > ~/.streamlit/config.toml
+
